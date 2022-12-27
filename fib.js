c76c2f9eaa31e936b731acfd27a3658065a6c186
lisichka2022 <99811204+lisichka2022@users.noreply.github.com>
2022-12-27 20:29:12 +0200

    Add files via upload

---
+++ fib.js
@@ -0,0 +1,14 @@
+'use strict'
+
+function fib(n=1){
+    let num1=1;
+    let num2=1;
+for(let i=3;i<=n;i++){
+let num3=num1+num2;
+num1=num2;
+num2=num3;
+}
+return num2;
+}
+
+console.log(fib(10));
